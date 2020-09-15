2e9971abaa172c5a2e4a6a13f5a86c0e37102e5f
Elzhas <esuleymenov@fil-it.ru>
2020-09-15 18:21:53 +0600

    started

---
+++ app/src/main/java/kz/sherua/nadoprodat/fragment/BasketFragment.kt
@@ -0,0 +1,26 @@
+package kz.sherua.nadoprodat.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
+import kz.sherua.nadoprodat.R
+
+class BasketFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v = inflater.inflate(R.layout.fragment_basket, container, false)
+        activity?.textView2?.text = "Корзина"
+        return v
+    }
+
+}
